a868b75648ce0aa8da1e43c95f2f37aad21bf440
sherlywu <sherlywu@outlook.com>
2021-01-14 20:43:52 +0800

    提交配置文件

--- config/config_data.py
+++ config/config_data.py
@@ -4,7 +4,7 @@
 
 # 定义可能要有的变量
 test_data = {'tid': '', 'reply_id': ''}
-test_token = 'abb32230-2853-41a7-9f0a-82c07a28c515'
+test_token = '739e3f44-4a25-4b39-a6a9-eade73dcbafa'
 loginname = 'fanmao1'
 msg_id = '58ec7d39da8344a81eee0c14'
 base_url = 'http://49.233.108.117:3000/api/v1'
